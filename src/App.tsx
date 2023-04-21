09e09c5b99f33c89f9e81dfd7447c5b5f9de9d3d
GDEIOANNES\gdeioannes <deioannes@mavon.art>
2023-04-21 02:47:29 +0200

    Small fucntionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useState } from "react";
+import "./App.css";
+import book from "./book.json";
+
+interface BookDic {
+  Germany: string;
+  English: string;
+  Spanish: string;
+}
 
 function App() {
+  let [searchWord, setSearchWord] = useState<BookDic[]>(book);
+  let inputWord: any = useRef();
+
+  function searchWordInJson() {
+    console.log(inputWord.current.value);
+    if (inputWord.current.value == "") {
+      setSearchWord(book);
+    } else {
+      let newList: BookDic[] = [];
+      book.map((word) => {
+        console.log();
+        if (
+          word.Germany.slice(0, inputWord.current.value.length) ==
+          inputWord.current.value
+        ) {
+          newList.push(word);
+        }
+      });
+
+      setSearchWord(newList);
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Kleine Prinz</h1>
+        <input
+          ref={inputWord}
+          onChange={() => {
+            searchWordInJson();
+          }}
+          type="text"
+        />
+
+        <div>
+          <h3>Word</h3>
+          <ul>
+            {searchWord.map((element) => {
+              return (
+                <>
+                  <div className="flip-card">
+                    <div className="flip-card-inner">
+                      <div className="flip-card-front">
+                        <p>{element.Germany}</p>
+                      </div>
+                      <div className="flip-card-back">
+                        <p>{element.English}</p>
+                        <br />
+                        <p>{element.Spanish}</p>
+                      </div>
+                    </div>
+                  </div>
+                </>
+              );
+            })}
+          </ul>
+        </div>
       </header>
     </div>
   );
